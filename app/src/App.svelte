bfda222110794b06aa6300216ff6cda1d32ed50c
Bruno Andozia <bruno.andozia@gmail.com>
2023-03-08 12:02:24 +0000

    app structure

---
+++ app/src/App.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	export let name: string;
+
+	const testFetch = () => {
+		fetch("https://www.google.com").then(res => {
+			res.text().then( data => {
+				console.log(data)
+				alert('working')
+			})
+		})
+	}
+
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button on:click={testFetch}>test fetch</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
